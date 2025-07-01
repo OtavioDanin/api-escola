227963c85c669f5ca77ce61fe6db1f1d8069dab8
Luiz Danin <otaviodanin@gmail.com>
2025-07-01 00:58:01 -0300

    Adicionando Autenticação nas rotas de aluno, turmas e status

--- routes/api.php
+++ routes/api.php
@@ -10,15 +10,15 @@ use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Support\Facades\Route;
 
 // Route::apiResource("aluno", AlunoController::class);
-Route::post('/aluno', [AlunoController::class, 'store'])->name('store');//->middleware(AlunoValidation::class);
-Route::put('/aluno/{idAluno}', [AlunoController::class, 'update'])->name('update');//->middleware(AlunoUpdateValidate::class)->middleware(JwtMiddleware::class);
-Route::get('/aluno', [AlunoController::class, 'index'])->name('index');//->middleware(JwtMiddleware::class);
-Route::get('/aluno/{idAluno}', [AlunoController::class, 'find'])->name('find');//->middleware(JwtMiddleware::class);
-Route::delete('/aluno/{idAluno}', [AlunoController::class, 'remove'])->name('remove');//->middleware(JwtMiddleware::class);
+Route::post('/aluno', [AlunoController::class, 'store'])->name('store')->middleware(AlunoValidation::class);
+Route::put('/aluno/{idAluno}', [AlunoController::class, 'update'])->name('update')->middleware(AlunoUpdateValidate::class)->middleware(JwtMiddleware::class);
+Route::get('/aluno', [AlunoController::class, 'index'])->name('index')->middleware(JwtMiddleware::class);
+Route::get('/aluno/{idAluno}', [AlunoController::class, 'find'])->name('find')->middleware(JwtMiddleware::class);
+Route::delete('/aluno/{idAluno}', [AlunoController::class, 'remove'])->name('remove')->middleware(JwtMiddleware::class);
 
 Route::put('/status/{idStatus}', [StatusController::class, 'update'])->name('update')->middleware(JwtMiddleware::class);
-Route::get('/status', [StatusController::class, 'index'])->name('index');
+Route::get('/status', [StatusController::class, 'index'])->name('index')->middleware(JwtMiddleware::class);
 
-Route::get('/turma', [TurmaController::class, 'index'])->name('index');
+Route::get('/turma', [TurmaController::class, 'index'])->name('index')->middleware(JwtMiddleware::class);
 
 Route::get('/auth', [AuthorizationController::class, 'generate'])->name('generate');
